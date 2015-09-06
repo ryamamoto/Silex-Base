7bf51e94896d02ade57747ee7fbe6605d77556e7
ryamamoto <ry116192j@gmail.com>
2015-09-06 23:36:23 +0900

    テスト追加

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Silex\WebTestCase;
+
+class ApplicationTest extends WebTestCase
+{
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../src/app.php';
+        require __DIR__.'/../config/prod.php';
+        require __DIR__.'/../src/controllers.php';
+
+        return $app;
+    }
+
+    public function test404()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/give-me-a-404');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function test200()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/');
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+}
